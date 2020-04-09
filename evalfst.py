7a242b31e1a16b8383f11e0ce3e236516b63b362
mhulden <mans.hulden@gmail.com>
2020-04-09 09:41:39 -0600

    Add lexicon-based ability

--- evalfst.py
+++ evalfst.py
@@ -1,5 +1,5 @@
 from foma import *
-import sys
+import sys, os
 
 def tagstobrackets(s):
     tags = s.split(';')
@@ -13,7 +13,11 @@ if mode == "-t":
 else:
     evalextension = ".dev"
 
+haslex = False
 T = FST.load(lang + ".fomabin")
+if os.path.isfile(lang + ".lex.fomabin"):
+    haslex = True
+    TL = FST.load(lang + ".lex.fomabin")
 
 evallines = [l.strip() for l in open(lang + evalextension)]
 
@@ -23,11 +27,18 @@ numcorrect = 0
 for l in evallines:
     lemma, form, tags = l.split('\t')
     tstring = tagstobrackets(tags)
-    guesses = list(T.apply_down(lemma + tstring))
-    if len(guesses) == 0:
-        guess = 'NO OUTPUT'
-    else:
-        guess = guesses[0] # should exist
+    haslexguess = False
+    if haslex: # Apply lexicon-based guess with priority
+        guesses = list(TL.apply_down(lemma + tstring))
+        if len(guesses) > 0:
+            haslexguess = True
+            guess = guesses[0]
+    if haslexguess == False:
+        guesses = list(T.apply_down(lemma + tstring))
+        if len(guesses) == 0:
+            guess = 'NO OUTPUT'
+        else:
+            guess = guesses[0]
     if guess == form:
         numcorrect += 1
     else:
